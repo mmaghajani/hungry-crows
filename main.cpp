eb39e633ca8010bb9c86f6ab18e518aaf38d0d75
mahdi aghajani <mma137421@gmail.com>
2016-11-02 17:24:15 +0330

    Adds print statement

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ using namespace std;
 
 int fillingNum = 9;
 int m, n, t;
+int awoker;
+int feeding = 0;
 sem_t restFood;
 sem_t notify;
 sem_t mutex;
@@ -25,24 +27,31 @@ void goto_sleep();
 void food_ready();
 
 void crow(void *threadid) {
+    printf("Baby crow %d started", threadid);
     while (true) {
         delay(PLAYING_TIME);
-        ready_to_eat();
+        ready_to_eat(threadid);
         delay(EATING_TIME);
+        printf("Baby crow %d is eating", threadid);
         finish_eating();
     }
 }
 
 void mother(void *threadid) {
-    while (t > 0) {
+    printf("Mother crow started");
+    while (feeding < t) {
         goto_sleep();
         delay(PREPARING_FOOD_TIME);
         food_ready();
         delay(WAITING_TIME);
     }
+    printf("Mother crow retires after serving %d feedings. Game ends!!!", feeding);
 }
 
 void food_ready() {
+    feeding++;
+    printf("Mother crow says \"Feeding (%d)\"", feeding);
+    printf("Mother crow is awoke by baby crow %d and starts preparing food.", awoker);
     for (int i = 0; i < m; i++)
         sem_post(&restFood);
     sem_wait(&mutex);
@@ -51,6 +60,7 @@ void food_ready() {
 }
 
 void goto_sleep() {
+    printf("Mother crow takes a nap");
     sem_wait(&notify);
 }
 
@@ -64,10 +74,13 @@ void finish_eating() {
     sem_post(&mutex);
 }
 
-void ready_to_eat() {
+void ready_to_eat(void *threadid) {
+    printf("Baby crow %d is ready to eat", threadid);
     sem_wait(&restFood);
     sem_wait(&mutex);
     if (fillingNum == 0) {
+        printf("Baby crow %d sees all feeding pots are empty and wakes up the mother.", threadid);
+        awoker = (int) threadid;
         sem_post(&notify);
         sem_wait(&restFood);
     }
@@ -76,9 +89,9 @@ void ready_to_eat() {
 
 int main(int argc, char *argv[]) {
     cin >> n >> m >> t;
-    if( n == 0 ) n = 10 ;
-    if( m == 0 ) m = 10 ;
-    if( t == 0 ) t = 10 ;
+    if (n == 0) n = 10;
+    if (m == 0) m = 10;
+    if (t == 0) t = 10;
 
     int NUM_CROWS = n;
     fillingNum = m - 1;
@@ -88,6 +101,8 @@ int main(int argc, char *argv[]) {
     sem_init(&notify, 0, 1);
     sem_init(&mutex, 0, 1);
 
+    printf("MAIN : There are %d baby crows, %d feeding pots , and %d feedings", n, m, t);
+    printf("MAIN : Game starts!!!!!");
     int rc;
     rc = pthread_create(&mother, NULL, crow, (void *) 0);
     if (rc) {
@@ -96,7 +111,6 @@ int main(int argc, char *argv[]) {
     }
 
     for (long t = 1; t <= NUM_CROWS; t++) {
-        printf("In main: creating thread %ld\n", t);
         rc = pthread_create(&crows[t], NULL, crow, (void *) t);
         if (rc) {
             printf("ERROR; return code from pthread_create() is %d\n", rc);
